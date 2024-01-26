336d5b940f95c1016f2303852322a3f197379b11
Tévin DERVAUX <tevin.dervaux@etu.univ-savoie.fr>
2024-01-26 12:00:47 +0100

    Onboardpage

--- Looter/OnboardingPageVIew.swift
+++ Looter/OnboardingPageVIew.swift
@@ -16,20 +16,36 @@ struct OnBoardingPageView: View {
     @State var imageRef: String
     @State var isBtnConfirm: Bool = false
     @Binding var currentPage: Int
+    @State var color: Color
     
     var body: some View {
         VStack{
             Text(title).font(.system(size: 40)).bold()
             Text(description)
-            Image(imageRef)
-            
-            if (currentPage == 3) {
-                Button("Confirmer") {
-                    isOnBoardingDone = true
-                }
-            } else {
-                Button("Suivant") {
-                    currentPage += 1
+            ZStack {
+                Circle()
+                    .frame(height: 200)
+                    .foregroundStyle(color)
+                Image(systemName: imageRef)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .foregroundStyle(Color(.white))
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .frame(width: 100, height: 50)
+                    .foregroundStyle(Color(.blue))
+                if (currentPage == 3) {
+                    Button("Confirmer") {
+                        isOnBoardingDone = true
+                    }
+                    .foregroundStyle(Color(.white))
+                } else {
+                    Button("Suivant") {
+                        currentPage += 1
+                    }
+                    .foregroundStyle(Color(.white))
                 }
             }
             
--- Looter/OnboardingView.swift
+++ Looter/OnboardingView.swift
@@ -13,13 +13,21 @@ struct OnboardingView: View {
     
     var body: some View {
         TabView(selection: $step) {
-            OnBoardingPageView(title: "Page1", description: "oe", imageRef: "co6996", currentPage: $step).tag(1)
-            OnBoardingPageView(title: "Page2", description: "oe", imageRef: "co6996", currentPage: $step).tag(2)
-            OnBoardingPageView(title: "Page3", description: "oe", imageRef: "co6996", currentPage: $step).tag(3)
+            OnBoardingPageView(title: "Page 1", description: "oe", imageRef: "gym.bag.fill", currentPage: $step, color: .blue).tag(1)
+            OnBoardingPageView(title: "Page 2", description: "oe", imageRef: "wand.and.stars", currentPage: $step, color: .yellow).tag(2)
+            OnBoardingPageView(title: "Page 3", description: "oe", imageRef: "iphone.case", currentPage: $step, color: .purple).tag(3)
         }.tabViewStyle(PageTabViewStyle())
     }
 }
 
 #Preview {
     OnboardingView()
+        .preferredColorScheme(.light)
+        .previewDisplayName("Light Mode")
+}
+
+#Preview {
+    OnboardingView()
+        .preferredColorScheme(.dark)
+        .previewDisplayName("Dark Mode")
 }
